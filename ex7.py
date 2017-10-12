dc8ab999b62d75466738f7fc5957dcbd1936485c
mrchooch <tyler-martignetti@hotmail.co.uk>
2017-10-12 11:00:42 +0100

    Add files via upload

--- ex7.py
+++ ex7.py
@@ -5,9 +5,16 @@ months = 0
 total_cost = float(input("Total cost of house: "))
 annual_salary = float(input("Annual Salary: "))
 portion_saved = float(input("% saved per month (as a float): "))
+semi_annual_raise = float(input("Semi Annual raise % (as a float): "))
 
 while current_savings < (total_cost*portion_deposit):
-    current_savings += ((current_savings * 0.04) / 12) + ((annual_salary / 12) * portion_saved)
-    months += 1
+	months += 1
+	if (months % 6 == 0):
+		annual_salary += semi_annual_raise
+		
+	current_savings += ((current_savings * 0.04) / 12) + ((annual_salary / 12) * portion_saved)
+
+
+
 
 print(months)
